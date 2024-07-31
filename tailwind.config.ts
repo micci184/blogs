43d19fc03406f03e6ca278d8c0e4e008e795bf54
micci184 <micci184@gmail.com>
2024-07-31 18:32:00 +0900

    mod lint and prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      maxWidth: {
+        "custom": "23rem",
+        "sm-custom": "23rem", // スマホ用のカスタム幅
+        "md-custom": "23rem",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,6 +20,7 @@ const config: Config = {
       },
     },
   },
+  darkMode: "class",
   plugins: [],
 };
 export default config;
